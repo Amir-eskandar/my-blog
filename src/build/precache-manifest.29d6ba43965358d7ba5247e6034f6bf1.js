4f68ae258316843c7d5c4e3d93bbe19f37e834bd
amireskandar <amirsobhylabib7@gmail.com>
2020-05-29 12:48:23 +0200

    my first commit

---
+++ src/build/precache-manifest.29d6ba43965358d7ba5247e6034f6bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4add1e37756a180b272523bd320a3b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f715a1a62acb9d859a30",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "599e2b75d46c61adba0a",
+    "url": "/static/js/2.67826c91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67826c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f715a1a62acb9d859a30",
+    "url": "/static/js/main.b3d2dc05.chunk.js"
+  },
+  {
+    "revision": "83a0a74cea62d7cb87f3",
+    "url": "/static/js/runtime-main.36b9964a.js"
+  }
+]);
